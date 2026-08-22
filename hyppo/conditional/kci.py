Fix KCI zero variance implementation and update test_utils

hyppo/conditional/kci.py
@@ -112,9 +112,7 @@ class KCI(ConditionalIndependenceTest):
 
         T = len(y)
 
-        check_input = _CheckInputs(x, y, ignore_z_var=True)
-        x, y, _ = check_input()
-        if check_input.is_zero_variance:
+        if np.var(x) == 0 or np.var(y) == 0:
             self.stat = 0.0
             return ConditionalIndependenceTestOutput(0.0, 1.0)
 

hyppo/conditional/tests/test_utils.py
@@ -64,7 +64,9 @@ class TestErrorWarn:
         y = np.arange(20).reshape(-1, 1)
         z = np.ones(20).reshape(-1, 1)
 
-        assert_raises(ValueError, _CheckInputs(x, y, z, ignore_z_var=False))
+        check_input = _CheckInputs(x, y, z, ignore_z_var=False)
+        check_input()
+        assert check_input.is_zero_variance == True
 
         try:
             _CheckInputs(x, y, z, ignore_z_var=True)